8a461890aa687d9e79568295f9aef13287e3caa1
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2025-03-13 16:53:39 +0900

    Update OpenSource.lua

--- OpenSource.lua
+++ OpenSource.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/OpenSourceScripts/refs/heads/main/MouseGlove.luau'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/OpenSourceScripts/refs/heads/main/Gloves/MouseGlove.luau'))()
